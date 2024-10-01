0dc57ede64fde8762367e27e3276304bdad6b426
octagony <moctagony@gmail.com>
2024-10-01 15:57:06 +0300

    [FEAT] Added basic container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+import { Container } from '@/components/site/Container/Container'
 import { useState } from 'react'
-import { Button } from './components/ui/button'
 
 function App() {
 	const [count, setCount] = useState(0)
 
 	return (
-		<>
-			<Button>Test shadcn</Button>
-		</>
+		<Container>
+			<h1>Container test</h1>
+		</Container>
 	)
 }
 
